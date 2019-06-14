4312b801fdd37f3eb5e8793c079d27264c2105d3
Sergey Vashchenko <vashenko49@gmail.com>
2019-06-15 00:54:37 +0300

    redid 3

--- javascript/script.js
+++ javascript/script.js
@@ -28,7 +28,6 @@ const ourServicesItemsInformation = [
 let ourServices = document.getElementById('ourServices');
 
 ourServices.onclick = function (event) {
-    debugger;
   let target =event.target;
   let children = ourServices.children;
   for(let i =0;i<ourServices.childElementCount;i++){
@@ -39,9 +38,173 @@ ourServices.onclick = function (event) {
         })
   }
   target.classList.add('service-items-link-check');
-  let elementCheck = target.children;
   document.getElementById('service-information-img').src = ourServicesItemsInformation[+target.dataset.number].img;
   document.getElementById('service-information-text').innerHTML =ourServicesItemsInformation[+target.dataset.number].text;
 
 
 };
+
+
+let  graphicDesign=['img/graphic_design/graphic-design1.jpg',
+    'img/graphic_design/graphic-design2.jpg',
+    'img/graphic_design/graphic-design3.jpg',
+    'img/graphic_design/graphic-design4.jpg',
+    'img/graphic_design/graphic-design5.jpg',
+    'img/graphic_design/graphic-design6.jpg',
+    'img/graphic_design/graphic-design7.jpg',
+    'img/graphic_design/graphic-design8.jpg',
+    'img/graphic_design/graphic-design9.jpg',
+    'img/graphic_design/graphic-design10.jpg',
+    'img/graphic_design/graphic-design11.jpg',
+    'img/graphic_design/graphic-design12.jpg',
+    'img/graphic_design/graphic-design1.jpg',
+    'img/graphic_design/graphic-design2.jpg',
+    'img/graphic_design/graphic-design3.jpg',
+    'img/graphic_design/graphic-design4.jpg',
+    'img/graphic_design/graphic-design5.jpg',
+    'img/graphic_design/graphic-design6.jpg',
+    'img/graphic_design/graphic-design7.jpg',
+    'img/graphic_design/graphic-design8.jpg',
+    'img/graphic_design/graphic-design9.jpg',
+    'img/graphic_design/graphic-design10.jpg',
+    'img/graphic_design/graphic-design11.jpg',
+    'img/graphic_design/graphic-design12.jpg',
+    'img/graphic_design/graphic-design1.jpg',
+    'img/graphic_design/graphic-design2.jpg',
+    'img/graphic_design/graphic-design3.jpg',
+    'img/graphic_design/graphic-design4.jpg',
+    'img/graphic_design/graphic-design5.jpg',
+    'img/graphic_design/graphic-design6.jpg',
+    'img/graphic_design/graphic-design7.jpg',
+    'img/graphic_design/graphic-design8.jpg',
+    'img/graphic_design/graphic-design9.jpg',
+    'img/graphic_design/graphic-design10.jpg',
+    'img/graphic_design/graphic-design11.jpg',
+    'img/graphic_design/graphic-design12.jpg',];
+let  webDesign=['img/web_design/web-design1.jpg',
+    'img/web_design/web-design2.jpg',
+    'img/web_design/web-design3.jpg',
+    'img/web_design/web-design4.jpg',
+    'img/web_design/web-design5.jpg',
+    'img/web_design/web-design6.jpg',
+    'img/web_design/web-design7.jpg',
+    'img/web_design/web-design1.jpg',
+    'img/web_design/web-design2.jpg',
+    'img/web_design/web-design3.jpg',
+    'img/web_design/web-design4.jpg',
+    'img/web_design/web-design5.jpg',
+    'img/web_design/web-design6.jpg',
+    'img/web_design/web-design7.jpg',
+    'img/web_design/web-design1.jpg',
+    'img/web_design/web-design2.jpg',
+    'img/web_design/web-design3.jpg',
+    'img/web_design/web-design4.jpg',
+    'img/web_design/web-design5.jpg',
+    'img/web_design/web-design6.jpg',
+    'img/web_design/web-design7.jpg',
+    'img/web_design/web-design1.jpg',
+    'img/web_design/web-design2.jpg',
+    'img/web_design/web-design3.jpg',
+    'img/web_design/web-design4.jpg',
+    'img/web_design/web-design5.jpg',
+    'img/web_design/web-design6.jpg',
+    'img/web_design/web-design7.jpg',
+    'img/web_design/web-design1.jpg',
+    'img/web_design/web-design2.jpg',
+    'img/web_design/web-design3.jpg',
+    'img/web_design/web-design4.jpg',
+    'img/web_design/web-design5.jpg',
+    'img/web_design/web-design6.jpg',
+    'img/web_design/web-design7.jpg',
+    'img/web_design/web-design1.jpg'];
+let  landingPages=['img/landing_page/landing-page1.jpg',
+    'img/landing_page/landing-page2.jpg',
+    'img/landing_page/landing-page3.jpg',
+    'img/landing_page/landing-page4.jpg',
+    'img/landing_page/landing-page5.jpg',
+    'img/landing_page/landing-page6.jpg',
+    'img/landing_page/landing-page7.jpg',
+    'img/landing_page/landing-page1.jpg',
+    'img/landing_page/landing-page2.jpg',
+    'img/landing_page/landing-page3.jpg',
+    'img/landing_page/landing-page4.jpg',
+    'img/landing_page/landing-page5.jpg',
+    'img/landing_page/landing-page6.jpg',
+    'img/landing_page/landing-page7.jpg',
+    'img/landing_page/landing-page1.jpg',
+    'img/landing_page/landing-page2.jpg',
+    'img/landing_page/landing-page3.jpg',
+    'img/landing_page/landing-page4.jpg',
+    'img/landing_page/landing-page5.jpg',
+    'img/landing_page/landing-page6.jpg',
+    'img/landing_page/landing-page7.jpg',
+    'img/landing_page/landing-page1.jpg',
+    'img/landing_page/landing-page2.jpg',
+    'img/landing_page/landing-page3.jpg',
+    'img/landing_page/landing-page4.jpg',
+    'img/landing_page/landing-page5.jpg',
+    'img/landing_page/landing-page6.jpg',
+    'img/landing_page/landing-page7.jpg',
+    'img/landing_page/landing-page1.jpg',
+    'img/landing_page/landing-page2.jpg',
+    'img/landing_page/landing-page3.jpg',
+    'img/landing_page/landing-page4.jpg',
+    'img/landing_page/landing-page5.jpg',
+    'img/landing_page/landing-page6.jpg',
+    'img/landing_page/landing-page7.jpg',
+    'img/landing_page/landing-page1.jpg'];
+let  wordpress=['img/wordpress/wordpress1.jpg',
+    'img/wordpress/wordpress2.jpg',
+    'img/wordpress/wordpress3.jpg',
+    'img/wordpress/wordpress4.jpg',
+    'img/wordpress/wordpress5.jpg',
+    'img/wordpress/wordpress6.jpg',
+    'img/wordpress/wordpress7.jpg',
+    'img/wordpress/wordpress8.jpg',
+    'img/wordpress/wordpress9.jpg',
+    'img/wordpress/wordpress10.jpg',
+    'img/wordpress/wordpress1.jpg',
+    'img/wordpress/wordpress2.jpg',
+    'img/wordpress/wordpress3.jpg',
+    'img/wordpress/wordpress4.jpg',
+    'img/wordpress/wordpress5.jpg',
+    'img/wordpress/wordpress6.jpg',
+    'img/wordpress/wordpress7.jpg',
+    'img/wordpress/wordpress8.jpg',
+    'img/wordpress/wordpress9.jpg',
+    'img/wordpress/wordpress10.jpg',
+    'img/wordpress/wordpress1.jpg',
+    'img/wordpress/wordpress2.jpg',
+    'img/wordpress/wordpress3.jpg',
+    'img/wordpress/wordpress4.jpg',
+    'img/wordpress/wordpress5.jpg',
+    'img/wordpress/wordpress6.jpg',
+    'img/wordpress/wordpress7.jpg',
+    'img/wordpress/wordpress8.jpg',
+    'img/wordpress/wordpress9.jpg',
+    'img/wordpress/wordpress10.jpg',
+    'img/wordpress/wordpress1.jpg',
+    'img/wordpress/wordpress2.jpg',
+    'img/wordpress/wordpress3.jpg',
+    'img/wordpress/wordpress4.jpg',
+    'img/wordpress/wordpress5.jpg',
+    'img/wordpress/wordpress6.jpg',];
+
+let workBlockButton = document.getElementById('work-block-button');
+
+let uploadingPictures = document.getElementById('uploading-pictures');
+let workBlockItems = document.getElementById('work-block-items');
+
+workBlockItems.onclick = function (event) {
+    let target =event.target;
+    let children = workBlockItems.children;
+    for(let i =0;i<workBlockItems.childElementCount;i++){
+        children[i].classList.forEach((elem)=>{
+            if(elem==='work-block-link-check'){
+                children[i].classList.remove('work-block-link-check');
+            }
+        })
+    }
+    target.classList.add('work-block-link-check');
+};
+
